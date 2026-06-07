3de6b04b44196eaf143850c71e4790a358e89227
Laurie <lespinasse.laurie5@gmail.com>
2026-06-07 02:42:53 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
+    styleUrl: './app.component.css'
+})
+export class AppComponent {}
